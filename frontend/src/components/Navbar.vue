f8596ef12c55d3498b86c5503fb40e249b7fa802
Nicolas Jimenez <niko12121212@gmail.com>
2024-07-01 18:50:38 -0400

    chore: Update Navbar component styling
    The code changes update the styling of the Navbar component by replacing the `space-x-4` class with `gap-4` for better spacing between elements. This improves the visual appearance of the Navbar and ensures consistent design across the application.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -14,7 +14,7 @@
                             icon="fa-solid fa-location-dot" />
                     </span> {{ direccion ? direccion : 'No disponible' }}</p>
             </div>
-            <div class="flex space-x-4 items-center">
+            <div class="flex gap-4 items-center">
                 <RouterLink to="/login" v-if="!cliente"
                     class="px-4 py-2 text-yellow-600 font-semibold rounded shadow-lg hover:bg-gray-200 transition-all duration-100">
                     <font-awesome-icon :icon="['fas', 'user']" /> Login
