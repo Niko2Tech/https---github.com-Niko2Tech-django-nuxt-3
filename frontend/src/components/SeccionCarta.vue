5dc75d8df48d9086223d76aeaa04cb37972c5334
Nicolas Jimenez <niko12121212@gmail.com>
2024-07-18 17:29:12 -0400

    Ajuste para que todo se mueva al centro

--- frontend/src/components/SeccionCarta.vue
+++ frontend/src/components/SeccionCarta.vue
@@ -2,7 +2,7 @@
     <section id="carta" class="container mx-auto my-5">
         <article v-for="(categoria, index) in data" :key="index">
             <h3 class="text-3xl font-bold text-center my-8">{{ categoria.nombre }}</h3>
-            <div class="flex flex-wrap gap-8">
+            <div class="flex flex-wrap gap-8 justify-center">
                 <div v-for="producto in categoria.productos" :key="producto.id">
                     <CardOferta :data="producto" />
                 </div>
